7a3f6498fa3351982aa4dd75f483738535bab1e4
niftyy <tusharsparte@gmail.com>
2020-01-16 17:22:24 +0530

    added initial files

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./App.css";
+import $ from "jquery";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  performSearch(searchTerm) {
+    const urlString =
+      "https://api.themoviedb.org/3/search/movie?&api_key=da17eed40cc1258d79d206c8a72880dc&language=en-US&page=1&include_adult=false&query=" +
+      searchTerm;
+  }
+
+  searchChangeHandler(event) {
+    console.log('Search query changed');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <table className="titleBar ">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" src="" />
+              </td>
+              <td width="8" />
+              <td>
+                <h3>MoviesDB Search</h3>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input
+          style={{
+            fontSize: 24,
+            display: "block",
+            width: "99%",
+            paddingTop: 8,
+            paddingBottom: 8,
+            paddingLeft: 16
+          }}
+          placeholder="Enter search term"
+          onChange={this.searchChangeHandler}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
