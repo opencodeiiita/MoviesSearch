9ff68a2cf71d992cfe5b988d8389f4d8d6dfda1f
Shreyas Gupta <technology.shreyas@gmail.com>
2020-01-18 10:07:54 +0530

    bind searchChangeHandler to this

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.searchChangeHandler = this.searchChangeHandler.bind(this);
   }
 
   performSearch(searchTerm) {
