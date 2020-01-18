0508ff5745e31aa5bec869c367d37f49e9f4739e
JhX <iit2019134@iiita.ac.in>
2020-01-18 12:22:59 +0530

    Added ES6 Class React Component

---
+++ public/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MovieRow extends React.Component {
+  render() {
+    return <h2>MovieRow</h2>;
+  }
+}
+
+ReactDOM.render(<MovieRow />, document.getElementById('MovieRow'));
