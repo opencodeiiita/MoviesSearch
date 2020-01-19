9a6e76fc3641e9c05cad27b7af157d7bf5a02b4f
JhX <iit2019134@iiita.ac.in>
2020-01-19 09:21:58 +0530

    Updated MovieRow Component

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -3,7 +3,33 @@ import ReactDOM from 'react-dom';
 
 class MovieRow extends React.Component {
   render() {
-    return <h2>MovieRow</h2>;
+    return(
+			<table>
+			<tr>
+				<th>Movie Poster</th>
+				<th>Details</th>
+			</tr>
+			<tr>
+				<td>[POSTER]</td>
+				<td>
+				<table>
+					<tr>
+						<th>Movie Name</th>
+					</tr>
+					<tr>
+						<td>[MOVIE NAME HERE]</td>
+					</tr>
+					<tr>
+						<th>Movie Description</th>
+					</tr>
+					<tr>
+						<td>[DESCRIPTION]</td>
+					</tr>
+				</table>
+				</td>
+			</tr>
+			</table>
+		  )
   }
 }
 
