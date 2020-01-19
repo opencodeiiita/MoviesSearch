89878bb0e57e8abffd628e4d5c6451b70c1e3c5e
Shreyas Gupta <technology.shreyas@gmail.com>
2020-01-19 09:46:40 +0530

    Loop over response pages and log results

--- src/App.js
+++ src/App.js
@@ -9,15 +9,31 @@ class App extends React.Component {
     this.state = {};
   }
 
-performSearch(searchTerm) {
+  performSearch(searchTerm) {
+    var data = []
     const urlString =
         "https://api.themoviedb.org/3/search/movie?&api_key=da17eed40cc1258d79d206c8a72880dc&language=en-US&page=1&include_adult=false&query=" +
-        searchTerm;
+        searchTerm + "&page=";
     $.ajax({
-        url: urlString,
+        url: urlString+"1",
         method: "GET"
     }).done(function(response) {
-        console.log(response);
+        data = data.concat(response.results)
+        if(response.total_pages>1) {
+          for(let i=2; i<=response.total_pages; i++) {
+            $.ajax({
+              url: urlString+i,
+              method: "GET"
+            }).done(function(resp) {
+              data = data.concat(resp.results)
+              if(i===response.total_pages) {
+                console.log(data);
+              }
+            }).fail(function() {
+              console.log("error")
+            });
+          }
+        }
     }).fail(function() {
         console.log("error")
     });
