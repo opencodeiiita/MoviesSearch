5f3717d15bcf4bc79cce9da46455eb72a91d6478
Shreyas Gupta <technology.shreyas@gmail.com>
2020-01-19 10:54:20 +0530

    Display only first page results

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends React.Component {
   }
 
   performSearch(searchTerm) {
-    var data = []
     const urlString =
         "https://api.themoviedb.org/3/search/movie?&api_key=da17eed40cc1258d79d206c8a72880dc&language=en-US&page=1&include_adult=false&query=" +
         searchTerm + "&page=";
@@ -18,22 +17,7 @@ class App extends React.Component {
         url: urlString+"1",
         method: "GET"
     }).done(function(response) {
-        data = data.concat(response.results)
-        if(response.total_pages>1) {
-          for(let i=2; i<=response.total_pages; i++) {
-            $.ajax({
-              url: urlString+i,
-              method: "GET"
-            }).done(function(resp) {
-              data = data.concat(resp.results)
-              if(i===response.total_pages) {
-                console.log(data);
-              }
-            }).fail(function() {
-              console.log("error")
-            });
-          }
-        }
+        console.log(response.results)
     }).fail(function() {
         console.log("error")
     });
