daafd6b051528db1c563648315e52607d27f9679
Shreyas Gupta <technology.shreyas@gmail.com>
2020-01-21 10:20:43 +0530

    Add social icons to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import "./App.css";
+import { SocialIcon } from 'react-social-icons';
 
 export default class Footer extends React.Component {
   render() {
     return (
         <div className="footer">
-            <p>&copy;2020 Movies Search</p>
+            <p className="copy">&copy;2020 OpenCode</p>
+            <ul className="social-icons">
+              <li><SocialIcon url="https://twitter.com/teamopencode" /></li>
+              <li><SocialIcon url="https://facebook.com/opencodeiiita" /></li>
+              <li><SocialIcon url="https://github.com/opencodeiiita" /></li>
+              <li><SocialIcon url="https://opencodeiiita.netlify.com/" /></li>
+            </ul>
         </div>
     );
   }
